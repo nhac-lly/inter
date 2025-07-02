41243e9c592ca8f74b3313ab2cbd98dd44df15d8
zat <fg42oh@gmail.com>
2025-07-02 14:26:28 +0700

    edit

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ const LivestreamViewer = dynamic(
   }
 );
 
+// Home Page
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
